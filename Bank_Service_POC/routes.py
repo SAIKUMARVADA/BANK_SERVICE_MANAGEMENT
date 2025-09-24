777a1c11baeba5923b5f65790e2b9a12a51a862e
SAIKUMAR VADA <saikumar.vada@thezennialpro.com>
2025-09-24 10:39:45 +0530

    Update routes.py

--- Bank_Service_POC/routes.py
+++ Bank_Service_POC/routes.py
@@ -33,36 +33,36 @@ router = APIRouter()
 
 # ----------------- Accounts -----------------
 @router.post("/account/create")
-def create_account_service(req: CreateAccountRequest):
+def create_account(req: CreateAccountRequest):
     return utility.create_account_service(req)
 
-@router.post("/account/balance")
-def check_balance_service(req: AccountRequest):
+@router.get("/account/balance")
+def check_balance(req: AccountRequest):
     return utility.check_balance_service(req)
 
-@router.post("/account/change_pin")
-def change_pin_service(req: ChangePinRequest):
+@router.put("/account/change_pin")
+def change_pin(req: ChangePinRequest):
     return utility.change_pin_service(req)
 
-@router.post("/account/kyc_update")
-def kyc_update_service(req: KYCUpdateRequest):
+@router.put("/account/kyc_update")
+def kyc_update(req: KYCUpdateRequest):
     return utility.kyc_update_service(req)
 
-@router.post("/account/close_account")
-def close_account_service(req: AccountRequest):
-    return utility.close_account_service(req)   
+@router.delete("/account/close_account")
+def close_account(req: AccountRequest):
+    return utility.close_account_service(req)
 
 # ----------------- Transactions -----------------
 @router.post("/transaction/deposit")
-def deposit_service(req: DepositRequest):
+def deposit(req: DepositRequest):
     return utility.deposit_service(req)
 
 @router.post("/transaction/withdraw")
-def withdraw_service(req: WithdrawRequest):
+def withdraw(req: WithdrawRequest):
     return utility.withdraw_service(req)
 
-@router.post("/transaction/transfer")
-def transfer_service(req: TransferRequest):
+@router.get("/transaction/transfer")
+def transfer(req: TransferRequest):
     return utility.transfer_service(req)
 
 # ----------------- Loans -----------------
@@ -70,6 +70,6 @@ def transfer_service(req: TransferRequest):
 def apply_loan(req: LoanRequest):
     return utility.apply_loan_service(req)
 
-@router.post("/loan/repay")
+@router.put("/loan/repay")
 def repay_loan(req: LoanRepayRequest):
     return utility.repay_loan_service(req)
